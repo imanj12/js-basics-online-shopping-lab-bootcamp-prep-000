1cc78ce5cb6ed4a7b3598192a33f22a16672d39c
Iman Jawad <imanj12@gmail.com>
2018-07-20 19:58:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,7 +65,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber == null) {
-    return 'Sorry, we don't have a 
+  if (cardNumber === null) {
+    return 'Sorry, we don\'t have a credit card on file for you.'
+  } else {
+    
   }
 }
