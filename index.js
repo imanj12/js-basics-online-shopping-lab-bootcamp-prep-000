be7f9754e84fae92fed0b129a8c3dc8bf5bbfcd9
Iman Jawad <imanj12@gmail.com>
2018-07-20 14:52:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function addToCart(itemName) {
  // write your code here
  var newItem = {}
  newItem.itemName = itemName
- newItem.itemPrice = 
+ newItem.itemPrice = Math.floor(Math.random())
  
 }
 
