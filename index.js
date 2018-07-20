3fe0e3a3e519913abc42bd730c2b41916af9762b
Iman Jawad <imanj12@gmail.com>
2018-07-20 19:44:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,16 +54,13 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  for (var i = 0; i <cart.length; i++) {
+  for (var i = 0; i < cart.length; i++) {
     if (cart[i].itemName === item) {
       cart.splice(i, 1)
+      return cart
     } else {
+      return 'That item is not in your cart.'
     }
-  }
-  if () {   //item is in cart
-    
-  } else {  //item is not in cart
-    
   }
 }
 
